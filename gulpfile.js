4b974d5025389afc7d41aae3672d4d6dccb9a475
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-07-24 21:06:14 +0200

    update gulp to use yarn

--- gulpfile.js
+++ gulpfile.js
@@ -150,14 +150,16 @@ gulp.task("clean:bundle", gulp.series("clean:output", "clean:dirs"));
 
 const install = async folder => {
   const directories = getDirs(folder);
-  const exec = promisify(childProcess.exec);
+  const execFile = promisify(childProcess.execFile);
 
   const promises = directories.map(async dir => {
     log.info(
       `Installing dependencies of ${clc.magenta(dir.replace(__dirname, ""))}`,
     );
     try {
-      await exec(`npm install --no-shrinkwrap --prefix ${dir}`);
+      await execFile(`yarn`, ["install"], {
+        cwd: dir,
+      });
     } catch (err) {
       log.error(`Failed installing dependencies of ${dir}`);
       throw err;
