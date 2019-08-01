58bb31bea96215905840651b6315ba951a25227e
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-08-01 13:36:55 +0200

    remove unneded comments

--- packages/odata-react/src/index.tsx
+++ packages/odata-react/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import ODataReact from "./ODataReact";
 import { PageWrapper } from "./components/styled/styled";
 import { mocks } from "./example/ODataFiles/index";
-// import { StyledOData } from "./CustomStyles";
 import "fiori-fundamentals/dist/fiori-fundamentals.min.css";
 
 ReactDOM.render(
