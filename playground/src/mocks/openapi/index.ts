a12d1850b1890d5cd367cbff1304d6e09704c30b
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-12-20 13:39:04 +0100

    Fix css styles

--- playground/src/mocks/openapi/index.ts
+++ playground/src/mocks/openapi/index.ts
@@ -2,10 +2,12 @@ import StringifiedJSON from "./string";
 import JSON from "./json";
 import YAMLV2 from "./yaml-v2";
 import YAMLV3 from "./yaml-v3";
+import LargeSpec from "./large-spec";
 
 export default {
   StringifiedJSON,
   JSON,
   YAMLV2,
   YAMLV3,
+  LargeSpec,
 };
